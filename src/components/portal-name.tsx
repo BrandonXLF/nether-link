1cd82e9c1c79f215df9f7292161a36f91d74388e
Brandon Fowler <brandfowl@gmail.com>
2025-02-07 16:58:36 -0500

    Show coordinates next to portal names

--- src/components/portal-name.tsx
+++ src/components/portal-name.tsx
@@ -1,5 +1,14 @@
+import Point from "@/classes/Point";
 import Portal from "@/classes/Portal";
 
 export default function PortalName({ portal }: Readonly<{ portal: Portal }>) {
-    return <span className={portal.isNether ? "text-red-900" : "text-green-700"}>{portal.name}</span>;
+    const color = portal.isNether ? "text-red-900" : "text-green-700";
+
+    if (!portal.name) {
+        return <span className={color}>{portal.toString()}</span>;
+    }
+
+    return <span>
+        <span className={color}>{portal.name}</span> <span className="text-sm">({Point.prototype.toString.call(portal)})</span>
+    </span>;
 }
