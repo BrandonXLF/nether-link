a7d6cfc9145e0eb94693c01fe06a15785552263e
Brandon Fowler <brandfowl@gmail.com>
2025-02-09 22:52:19 -0500

    Use Box for the main heading

--- src/components/box.tsx
+++ src/components/box.tsx
@@ -1,12 +1,16 @@
 import { ReactNode } from "react";
 
-export default function Box({ className, title, children }: Readonly<{
+export default function Box({ className, level, title, children }: Readonly<{
 	className?: string,
-	title: string,
+	level?: 1 | 2,
+	title?: string,
 	children: ReactNode
 }>) {
+	const HeadingTag = `h${level ?? 2}` as const;
+	const headingSize = level == 1 ? 'text-3xl' : 'text-lg';
+
 	return <div className={`${className ?? ''} p-4`}>
-		<h2 className="text-lg font-semibold mb-3">{title}</h2>
+		{title && <HeadingTag className={`${headingSize} font-semibold mb-3`}>{title}</HeadingTag>}
 		{children}
 	</div>;
 }
