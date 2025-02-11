6070445eeda86de95c2e632ca1763359f5c98034
Brandon Fowler <brandfowl@gmail.com>
2025-02-11 14:05:03 -0500

    Set `trailingSlash: true`

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   assetPrefix: './',
+  trailingSlash: true,
   reactStrictMode: true,
   images: {
     unoptimized: true
